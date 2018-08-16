bed03577651c34aba6e3c43e56e817b8e50bbea9
ANDREI VEREMENKO <C0C1CbKA@gmail.com>
2018-08-16 22:12:09 +0300

    ДЗ 5
    выполнено без доп задания

--- src/index.js
+++ src/index.js
@@ -33,9 +33,7 @@ function removeListener(eventName, target, fn) {
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
-	target.addEventListener(eventName, function() {
-		this.preventDefault();
-	});
+	target.addEventListener(eventName, (e) => e.preventDefault());
 }
 
 /*
@@ -46,7 +44,9 @@ function skipDefault(eventName, target) {
  Пример:
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
-function emulateClick(target) {}
+function emulateClick(target) {
+	target.click();
+}
 
 /*
  Задание 5:
@@ -57,7 +57,9 @@ function emulateClick(target) {}
  Пример:
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
-function delegate(target, fn) {}
+function delegate(target, fn) {
+	target.querySelector('button').addEventListener('click', fn);
+}
 
 /*
  Задание 6:
@@ -68,7 +70,14 @@ function delegate(target, fn) {}
  Пример:
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
-function once(target, fn) {}
+function once(target, fn) {
+	let temp_func = () => {
+		fn();
+		target.removeEventListener('click', temp_func);
+	};
+
+	target.addEventListener('click',temp_func);
+}
 
 export {
 	addListener,
