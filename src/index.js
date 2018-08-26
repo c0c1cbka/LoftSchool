8e4a94e9fd877f6736b9fd15a11b511b01389f56
ANDREI VEREMENKO <C0C1CbKA@gmail.com>
2018-08-26 10:34:50 +0300

    ДЗ 6 v0.1

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
-	return new Promise((resolved, reject)=>{
-		setTimeout(resolved ,seconds*1000);
+	return new Promise((resolved, reject) => {
+		setTimeout(resolved, seconds * 1000);
 	});
 }
 
@@ -21,25 +21,30 @@ function delayPromise(seconds) {
 
  Массив городов пожно получить отправив асинхронный запрос по адресу
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
-
+ 
  2.2: Элементы полученного массива должны быть отсортированы по имени города
 
  Пример:
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
-	/*return new Promise((resolved, reject)=>{
-		var req = new XMLHttpRequest();
+	return new Promise((resolved, reject) => {
+		let req = new XMLHttpRequest();
 		req.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json', true);
-		req.onload = ()=>{resolved(req.response.sort());console.log(req.response);};
-	});*/
-
-	var req = new XMLHttpRequest();
-		req.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json', true);
-		req.onload = ()=> console.log(req.response);
+		req.send();
+		req.addEventListener('load', () => {
+			let res = [];
+			JSON.parse(req.response, (key, val) => {
+				if (key === 'name') {
+					res.push(val);
+				}
+			});
+			resolved(res.sort());
+		});
+	});
 }
 
 export {
-    delayPromise,
-    loadAndSortTowns
+	delayPromise,
+	loadAndSortTowns
 };
